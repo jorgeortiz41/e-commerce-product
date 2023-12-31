fe5b822a02efff35e3462dccd88850399f43c245
jorgeortiz41 <jorge.ortiz41@upr.edu>
2023-12-31 19:15:04 -0400

    changed nav to separate component

--- app/page.js
+++ app/page.js
@@ -2,14 +2,12 @@
 
 import Image from 'next/image'
 import { useState } from 'react'
+import Nav from './_components/nav'
 
 export default function Home() {
   const [cart, setCart] = useState([])
-  const [menu, setMenu] = useState(false)
   const [quantity, setQuantity] = useState(1)
   const [image, setImage] = useState('/image-product-1.jpg')
-  const [showCart, setShowCart] = useState(false)
-  const [totalPrice, setTotalPrice] = useState(0)
   
 
   const incrementQuantity = () => {
@@ -26,24 +24,22 @@ export default function Home() {
     setImage(newImage)
   }
 
-  //TODO: fix totalPrice
   const addToCart = () => {
-    setTotalPrice(quantity * 125);
     const newItem = {
       image: '/image-product-1.jpg',
       name: 'Fall Limited Edition Sneakers',
       quantity: quantity,
       price: 125.00,
-      totalPrice: totalPrice
+      totalPrice: quantity * 125.00
     };
   
     setCart(prevCart => {
       const existingItem = prevCart.find(item => item.name === newItem.name);
       if (existingItem) {
-        // If the item already exists in the cart, update the quantity
+        // If the item already exists in the cart, update the quantity and totalPrice
         return prevCart.map(item =>
           item.name === newItem.name
-            ? { ...item, quantity: item.quantity + newItem.quantity }
+            ? { ...item, quantity: item.quantity + newItem.quantity,totalPrice: item.totalPrice + newItem.totalPrice }
             : item
         );
       } else {
@@ -53,91 +49,10 @@ export default function Home() {
     });
   };
 
-  const toggleCart = () => {
-    setShowCart(!showCart)
-  }
-
   return (
     <div className='flex flex-col mx-24 h-dvh'>
-      <nav className='flex flex-row justify-between h-32 border-b-[1px] border-gray-200'>
-        <div id="left-nav" className='flex flex-row gap-12'>
-          <div id='logo' className='relative w-28'>
-            <Image
-            className=''
-            src="/logo.svg"
-            alt="cart"
-            fill
-            style={{objectFit: "contain"}}
-            />
-          </div>
-        
-          <button className='text-gray-500 hover:text-gray-700 text-sm border-b-4 border-transparent hover:border-orange-400'>Collections</button>
-          <button className='text-gray-500 hover:text-gray-700 text-sm border-b-4 border-transparent hover:border-orange-400'>Men</button>
-          <button className='text-gray-500 hover:text-gray-700 text-sm border-b-4 border-transparent hover:border-orange-400'>Women</button>
-          <button className='text-gray-500 hover:text-gray-700 text-sm border-b-4 border-transparent hover:border-orange-400'>About</button>
-          <button className='text-gray-500 hover:text-gray-700 text-sm border-b-4 border-transparent hover:border-orange-400'>Contact</button>
-        </div>
-
-        <div id="right-nav" className='flex flex-row gap-12 items-center'>
-          <button id='nav-cart' className='relative w-12 h-12' onClick={toggleCart}>
-            {cart.length > 0 && (
-              <span className=" absolute top-0 right-4 z-10 inline-flex items-center rounded-lg bg-orange-400 px-1 py-0.5 text-xs font-medium text-white">{cart[0].quantity}</span>
-            )}
-            {showCart && (
-              <div className='absolute top-12 right-0 w-80 h-64 bg-white rounded-md shadow-lg'>
-                <div className='flex flex-col gap-4'>
-                  <div className='flex justify-start items-center text-sm font-bold border-b-[1px] border-gray-200 p-4 pb-6'>
-                    Cart
-                  </div>
-                  <div className='flex flex-row justify-center items-center'>
-                    <div className='flex flex-row gap-4 items-center'>
-                      {cart.length > 0 ? 
-                      <div className='flex flex-col gap-4'>
-                        {cart.map((item, index) => (
-                          <div key={index} className='flex flex-row gap-2 p-4 items-center'>
-                            <div className='relative w-12 h-12'>
-                              <Image
-                              className='rounded-lg'
-                              src={item.image}
-                              alt="cart"
-                              fill
-                              />
-                            </div>
-                            <div className='flex flex-col gap-1'>
-                              <div className='text-xs text-gray-500'>{item.name}</div>
-                              <div className='flex flex-row gap-2'>
-                              <div className='text-xs text-gray-500'>${item.price}.00 x {item.quantity}</div>
-                              <div className='text-xs'>{item.totalPrice}</div>
-                              </div>
-                            </div>
-                          </div>
-                        ))}
-                      </div>
-                      :<div className='flex text-sm text-gray-500 justify-center'>Your cart is empty</div>}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              )}
-            <Image
-              className='z-5'
-              src="/icon-cart.svg"
-              alt="cart"
-              height={24}
-              width={24}
-            />
-          </button>
-          <button id='nav-avatar' className='relative rounded-full w-12 h-12 hover:ring-2 ring-orange-400'>
-            <Image
-            className='rounded-full'
-            src="/image-avatar.png"
-            alt="Picture of sneakers"
-            fill
-            />
-          </button>
-        </div>
-        
-      </nav>
+      
+      <Nav cart={cart} />
 
       <main id='content' className='flex md:flex-row flex-col p-12 gap-16 h-dvh'>
 
@@ -222,7 +137,7 @@ export default function Home() {
               </button>
             </div>
 
-            <button className='flex flex-row gap-4 items-center h-14 px-4 rounded-md bg-orange-400 w-80 justify-center shadow-lg shadow-orange-400/50 hover:opacity-50' onClick={addToCart}>
+            <button className='flex flex-row gap-4 items-center h-14 px-4 rounded-md bg-orange-400 w-64 justify-center shadow-lg shadow-orange-400/50 hover:opacity-50' onClick={addToCart}>
                 <Image
                 className=''
                 src="/icon-cart-white.svg"
