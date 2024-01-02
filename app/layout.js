7962a3d5585d9f31d401caf7b5ea0a367db4174a
jorgeortiz41 <jorge.ortiz41@upr.edu>
2024-01-02 10:08:18 -0400

    format document

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
-import { Inter, Azeret_Mono } from 'next/font/google'
-import './globals.css'
+import { Inter, Azeret_Mono } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
-const azeret = Azeret_Mono({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+const azeret = Azeret_Mono({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Sneakers',
-  description: 'Example e-commerce product page',
-}
+  title: "Sneakers",
+  description: "Example e-commerce product page",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={azeret.className}>{children}</body>
     </html>
-  )
+  );
 }
